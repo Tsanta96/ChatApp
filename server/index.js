5432a8b8c8560ea644a179b603a09292e874423c
Matteo Santavicca <matteosantavicca12@gmail.com>
2020-04-20 17:13:15 -0400

    Add socketio backend setup in server/index.js

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+
+const PORT = process.env.PORT || 5000;
+
+const router = require('./router');
+
+const app = express();
+const server = http.createServer(app);
+//instance of socket.io
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log("We have a new connection!!!");
+
+    socket.on('disconnect', () => {
+        console.log("User left!!!");
+    })
+})
+
+//calling as middleware
+app.use(router);
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
