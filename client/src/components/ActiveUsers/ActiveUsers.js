867921338ddb78f6067572fa3171fc6a9c1f6e89
Matteo Santavicca <matteosantavicca@matteos-mbp.lan>
2025-04-22 18:11:42 -0700

    Add an alt to the img tag

--- client/src/components/ActiveUsers/ActiveUsers.js
+++ client/src/components/ActiveUsers/ActiveUsers.js
@@ -1,41 +1,45 @@
-import React, { useState, useRef } from 'react';
-import './ActiveUsers.css';
+import React, { useState, useRef } from "react";
+import "./ActiveUsers.css";
 
-import downArrowIcon from '../../icons/down-arrow.png';
-import upArrowIcon from '../../icons/up-arrow.png';
+import downArrowIcon from "../../icons/down-arrow.png";
+import upArrowIcon from "../../icons/up-arrow.png";
 
 const ActiveUsers = ({ users }) => {
-	const [dropDownActive, setDropDownActive] = useState(false);
-	const dropDownRef = useRef();
+  const [dropDownActive, setDropDownActive] = useState(false);
+  const dropDownRef = useRef();
 
-	const handleDropDown = () => {
-		if (dropDownActive === false) setDropDownActive(true);
-		else setDropDownActive(false);
-	}
+  const handleDropDown = () => {
+    if (dropDownActive === false) setDropDownActive(true);
+    else setDropDownActive(false);
+  };
 
-	return (
-		<div className="usersContainer">
-			<div className={"activeUsersHeader"} onClick={handleDropDown}>
-				<img 
-					src = { dropDownActive ? upArrowIcon : downArrowIcon }
-					className="downArrowIcon" />
-				<h1 className="activeUsersHeaderText">Active Users</h1>
-			</div>
-			<div className="nameBox">
-				<div 
-					className={dropDownActive ? "names" : "names hidden"}
-					ref={dropDownRef}
-					onClick={handleDropDown}
-				>
-					<div className="innerNameBox">
-						{users.map((user, idx) => (
-								<div key={idx}>{idx+1} {user}</div>
-						))}
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="usersContainer">
+      <div className={"activeUsersHeader"} onClick={handleDropDown}>
+        <img
+          src={dropDownActive ? upArrowIcon : downArrowIcon}
+          alt="Down Arrow Icon"
+          className="downArrowIcon"
+        />
+        <h1 className="activeUsersHeaderText">Active Users</h1>
+      </div>
+      <div className="nameBox">
+        <div
+          className={dropDownActive ? "names" : "names hidden"}
+          ref={dropDownRef}
+          onClick={handleDropDown}
+        >
+          <div className="innerNameBox">
+            {users.map((user, idx) => (
+              <div key={idx}>
+                {idx + 1} {user}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ActiveUsers;
--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,52 +1,52 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
-import peachIcon from '../../icons/PeachFalcRed.png';
+import peachIcon from "../../icons/PeachFalcRed.png";
 
 //Refactor styles using Grid and SCSS
-import './Join.css';
+import "./Join.css";
 
 // When user joins, a connection event is fired off to backend
 // When user leaves, a disconnect event is fired off to backend
 
 const Join = () => {
-	const [name, setName] = useState('');
-	const [room, setRoom] = useState('');
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
 
-	return (
-		<div className="joinOuterContainer"> 
-			<img 
-				src={peachIcon}
-				className="peachImage"
-			/>
-			<p className="chatHeader">Peachtree Chats</p>
-			<div className="chatHeaderUnderline"></div>
-			<div className="joinInnerContainer">
-				<h1 className="heading">Join</h1>
-				<div>
-					<input 
-						placeholder="Name"
-						className="joinInput"
-						type="text"
-						onChange={(event) => setName(event.target.value)}
-					/>
-				</div>
-				<div>
-					<input 
-						placeholder="Room"
-						className="joinInput mt-20"
-						type="text"
-						onChange={(event) => setRoom(event.target.value)}
-					/>
-				</div>
-				<Link 
-					onClick={(event) => (!name || !room) ? event.preventDefault() : null}
-					to={`/chat?name=${name}&room=${room}`}>
-					<button className="button mt-20" type="submit">Sign In!</button>
-				</Link>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div className="joinOuterContainer">
+      <img src={peachIcon} alt="Peachtree Chats icon" className="peachImage" />
+      <p className="chatHeader">Peachtree Chats</p>
+      <div className="chatHeaderUnderline"></div>
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            placeholder="Name"
+            className="joinInput"
+            type="text"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Room"
+            className="joinInput mt-20"
+            type="text"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={(event) => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button mt-20" type="submit">
+            Sign In!
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Join;
