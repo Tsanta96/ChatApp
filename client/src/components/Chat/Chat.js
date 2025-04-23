dcfb1d653826fc90d373096edff87806d8984a80
Matteo Santavicca <matteosantavicca@matteos-mbp.lan>
2025-04-23 12:27:27 -0700

    Update endpoint name and setback project name

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,74 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import queryString from 'query-string';
-import io from 'socket.io-client';
-import './Chat.css';
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import "./Chat.css";
 
-import InfoBar from '../InfoBar/InfoBar';
-import Input from '../Input/Input';
-import Messages from '../Messages/Messages';
-import ActiveUsers from '../ActiveUsers/ActiveUsers';
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import ActiveUsers from "../ActiveUsers/ActiveUsers";
 
 let socket;
 
 //location comes from 'router'
 const Chat = ({ location }) => {
-	const [name, setName] = useState('');
-	const [room, setRoom] = useState('');
-	const [message, setMessage] = useState('');
-	const [messages, setMessages] = useState([]);
-	const [users, setUsers] = useState([]);
-	const ENDPOINT = 'https://react-mychatapp.herokuapp.com/';
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+  const ENDPOINT =
+    process.env.NODE_ENV === "production"
+      ? "https://peachtree-chats-15c4ca881dfb.herokuapp.com/"
+      : "http://localhost:5001";
 
-	useEffect(() => {
-		const { name, room } = queryString.parse(location.search)
-		socket = io(ENDPOINT);
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    socket = io(ENDPOINT);
 
-		setName(name);
-		setRoom(room);
+    setName(name);
+    setRoom(room);
 
-		socket.emit('join', { name, room }, ({ error }) => { alert(error) });
+    socket.emit("join", { name, room }, ({ error }) => {
+      alert(error);
+    });
 
-		//Used for unmounting
-		return () => {
-			socket.emit('disconnect');
-			//turns off socket instance
-			socket.off();
-		};
-	}, [ENDPOINT, location.search]);
+    //Used for unmounting
+    return () => {
+      socket.emit("disconnect");
+      //turns off socket instance
+      socket.off();
+    };
+  }, [ENDPOINT, location.search]);
 
-	useEffect(() => {
-		socket.on('message', (message) => {
-			setMessages([...messages, message])
-		})
-	}, [messages]);
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
 
-	useEffect(() => {
-		socket.on('roomData', (roomData) => {
-			let users = roomData.users.map((user) => user.name);
-			setUsers(users);
-		})
-	}, [users]);
+  useEffect(() => {
+    socket.on("roomData", (roomData) => {
+      let users = roomData.users.map((user) => user.name);
+      setUsers(users);
+    });
+  }, [users]);
 
-	const sendMessage = (event) => {
-		//When you key press or button click an entire page refresh happens
-		//event.preventDefault keeps this full page refresh from happening
-		event.preventDefault();
+  const sendMessage = (event) => {
+    //When you key press or button click an entire page refresh happens
+    //event.preventDefault keeps this full page refresh from happening
+    event.preventDefault();
 
-		if (message) socket.emit('sendMessage', message, () => setMessage(''))
-	}
-
-	console.log(message, messages);
-
-	return (
-		<div className="outerContainer">
-			<div className="container">
-				<InfoBar room={room}/>
-				<Messages messages={messages} name={name}/>
-				<Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-			</div>
-			<ActiveUsers users={users}/>
-		</div>
-	)
-}
+    if (message) socket.emit("sendMessage", message, () => setMessage(""));
+  };
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <ActiveUsers users={users} />
+    </div>
+  );
+};
 
 export default Chat;
--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const server = http.createServer(app);
 //instance of socket.io
 const io = socketio(server, {
   cors: {
-    origin: "https://react-mychatapp.herokuapp.com",
+    origin: "https://peachtree-chats-15c4ca881dfb.herokuapp.com/",
     methods: ["GET", "POST"],
   },
 });
